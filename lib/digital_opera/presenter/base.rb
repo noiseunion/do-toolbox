e4c61f9be8cd3bff0242ec221f08d9b2c49cedac
JD Hendrickson <jd@digitalopera.com>
2013-11-08 17:53:53 -0600

    added presenter::base class

--- lib/digital_opera/presenter/base.rb
+++ lib/digital_opera/presenter/base.rb
@@ -6,7 +6,14 @@
 module DigitalOpera
   module Presenter
     class Base < SimpleDelegator
+      def initialize(base, view_context=nil)
+        super(base)
+        @view_context = view_context || ActionController::Base.new.view_context
+      end
 
+      def _h
+        @view_context
+      end
     end
   end
 end
